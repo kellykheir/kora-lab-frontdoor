359d0598cda6fe6f7dee2eb28a8db61256745fe5
Kheir Lissi <juniorkheir@gmail.com>
2026-05-26 11:24:56 -0400

    Hamburger: inline style au lieu de classe Tailwind pour fond opaque

--- src/components/site-layout.tsx
+++ src/components/site-layout.tsx
@@ -70,7 +70,7 @@ function Nav() {
         </button>
       </div>
       {open && (
-        <div className="fixed inset-0 top-16 z-40 flex flex-col gap-6 bg-ink px-6 py-12 md:hidden">
+        <div className="fixed inset-0 top-16 z-40 flex flex-col gap-6 px-6 py-12 md:hidden" style={{ backgroundColor: "#0A0A0A" }}>
           {NAV_LINKS.map((l) => (
             <Link
               key={l.to}
